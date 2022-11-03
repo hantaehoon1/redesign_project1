ef6671cacd2f9b419d8320e667d1007e467acee8
hantaehoon1 <hantaehoon1@naver.com>
2022-11-03 15:30:37 +0900

    슬라이더 수정

--- javacript/script.js
+++ javacript/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-
+  
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",

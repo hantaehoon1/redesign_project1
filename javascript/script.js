fe957884cf48541a9d0e828b2d376425f6fea7c7
hantaehoon1 <hantaehoon1@naver.com>
2022-11-07 15:32:40 +0900

    2022-11-07 카드수정

---
+++ javascript/script.js
@@ -0,0 +1,37 @@
+function Imgslide1_init(){
+  const swiper = new Swiper(".imgslide .swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+
+  swiper.on("slideChange", function () {
+    console.log(swiper.activeIndex);
+    
+    $(".imgslide").removeClass("first-slide-actived");
+
+    if(swiper.activeIndex == 1 || swiper.activeIndex ==4) {
+      $(".imgslide").addClass("first-slide-actived");
+    }
+  
+  });
+
+}
+
+Imgslide1_init();
